12b0168c4e689129cb4cb6bef91c570d3e5f84e9
Jamie Bort <jamiebort@gmail.com>
2021-05-26 19:49:49 -0400

    Modified the frontend/personal_dashboard/src/App.js file.

--- frontend/personal_dashboard/src/App.js
+++ frontend/personal_dashboard/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Welcome  from "./components/Welcome";
+import NavButton from "./components/NavButton";
+
+function buttonFunction(){
+  console.log("button is fired");
+}
+
+const secondButt= function secondButtonFunction(){
+  console.log("button is fired");
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +27,12 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Welcome name="Jamie"/>
+      <NavButton abc="first page"/>
+      <button onClick={()=>{console.log("yeah!"); buttonFunction();}}>A button</button>
+      <NavButton abc="second page" bf={secondButt}/>
+      <NavButton abc="third page"/>
     </div>
   );
 }
