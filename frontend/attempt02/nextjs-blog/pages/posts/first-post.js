0b693f59cb38f84f18b1144545030ef976eb24ad
Jamie Bort <jamiebort@gmail.com>
2021-06-01 10:57:45 -0400

    Complete Assets, Metadata, and CSS lesson in the frontend/attempt02/nextjs-blog/ directory.

--- frontend/attempt02/nextjs-blog/pages/posts/first-post.js
+++ frontend/attempt02/nextjs-blog/pages/posts/first-post.js
@@ -2,17 +2,62 @@
 //     return <h1>First Post</h1>
 //   }
 
+// ==========================================
+
+// import Link from 'next/link'
+
+// export default function FirstPost() {
+//   return (
+//     <>
+//       <h1>First Post</h1>
+//       <h2>
+//         <Link href="/">
+//           <a>Back to home</a>
+//         </Link>
+//       </h2>
+//     </>
+//   )
+// }
+
+// ==========================================
+
+// import Link from 'next/link'
+// import Head from 'next/head'
+
+// export default function FirstPost() {
+//     return (
+//       <>
+//         <Head>
+//           <title>First Post</title>
+//         </Head>
+//         <h1>First Post</h1>
+//         <h2>
+//           <Link href="/">
+//             <a>Back to home</a>
+//           </Link>
+//         </h2>
+//       </>
+//     )
+//   }
+
+// ==========================================
+
 import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
-  return (
-    <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </>
-  )
-}
+    return (
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </h2>
+      </Layout>
+    )
+  }
